7f89d54032217d33731445f27ff32539320b567f
jesusjimsa <jesusjimsa@icloud.com>
2021-07-06 21:05:08 +0100

    Creating the buttons and results field

--- iOS-Calculator/AppDelegate.swift
+++ iOS-Calculator/AppDelegate.swift
@@ -12,12 +12,25 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+
+        // Setup
+        setupView()
+
         return true
     }
 
-    // MARK: UISceneSession Lifecycle
+    // MARK: Private methods
+
+    private func setupView() {
+        let vc = HomeViewController()
+
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = vc
+        window?.makeKeyAndVisible()
+    }
 
 
 }
---
+++ iOS-Calculator/Controller/Home/HomeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  HomeViewController.swift
+//  iOS-Calculator
+//
+//  Created by Jesús Jiménez Sánchez on 6/7/21.
+//
+
+import UIKit
+
+final class HomeViewController: UIViewController {
+
+    //MARK: Initialization
+
+    init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: Life Cycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
