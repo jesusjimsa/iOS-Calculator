2de18e513ac1b94a638d591865370d5117cbb992
jesusjimsa <jesusjimsa@icloud.com>
2021-07-06 20:08:19 +0100

    Initial files

---
+++ iOS-Calculator/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  iOS-Calculator
+//
+//  Created by Jesús Jiménez Sánchez on 30/6/21.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+    // MARK: UISceneSession Lifecycle
+
+
+}
+
